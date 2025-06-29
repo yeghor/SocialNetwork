fc77e77bab12e2bd180c1ef255e1d8b75d7427ca
RomanovYehor <yeghor.romanov@gmail.com>
2025-06-29 14:54:25 +0300

    Add password manager. Cover with tests

---
+++ backend/authorization/auth_tests.py
@@ -0,0 +1,13 @@
+import pytest
+from password_manager import hash_password, check_password
+
+
+@pytest.fixture
+def prepared_pws() -> tuple:
+    """Indexes: 0 - Correct password | 1 - Wrong password | 2 - Hash of correct password"""
+    return ("password", "wrongpassword", hash_password("password"))
+
+
+def test_pw_hashing(prepared_pws):
+    assert check_password(prepared_pws[0], prepared_pws[2]) == True
+    assert check_password(prepared_pws[1], prepared_pws[2]) == False
---
+++ backend/authorization/password_manager.py
@@ -0,0 +1,12 @@
+import bcrypt
+
+def hash_password(raw_pass: str) -> str:
+    salt = bcrypt.gensalt()
+    bytes = raw_pass.encode()
+    hashed_password = bcrypt.hashpw(bytes, salt)
+    return hashed_password.decode()
+
+def check_password(entered_pass: str, hashed_pass: str) -> bool:
+    return bcrypt.checkpw(entered_pass.encode(), hashed_pass.encode())
+
+hashed_pw = hash_password('password')
