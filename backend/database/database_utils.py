9007be711e127e8742a759657a7bb5699afcd5c2
RomanovYehor <yeghor.romanov@gmail.com>
2025-07-04 14:11:32 +0300

    Added database error handler decorator

--- backend/database/database_utils.py
+++ backend/database/database_utils.py
@@ -1,9 +1,33 @@
 from main import engine, SessionLocal
 from sqlalchemy.ext.asyncio import AsyncSession
+from functools import wraps
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException
+from models import Base
 
 async def get_session_depends():
-    async with SessionLocal() as db:
+    async with SessionLocal() as conn:
         try:
-            yield db
+            yield conn
         finally:
-            await db.close()
+            await conn.close()
+
+def database_error_handler(action: str = "Uknown action with the database"):
+    async def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            try:
+                return await func(*args, **kwargs)
+            except Exception as e:
+                if isinstance(e, SQLAlchemyError):
+                    raise HTTPException(
+                        status_code=500,
+                        detail=f"Error with the database occured: {e} | Action: {action}"
+                    )
+
+                raise HTTPException(
+                    status_code=500,
+                    detail=F"Unkown error with the database occured: {e} | Action: {action}"
+                )
+        return wrapper
+    return decorator
