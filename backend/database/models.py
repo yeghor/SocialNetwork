b89eb3ea63bc60b446c8abe163e097f42608f44b
RomanovYehor <yeghor.romanov@gmail.com>
2025-07-05 15:56:42 +0300

    Changed the database structure, made it similar to twitter (now :( ) post and replies. Added field validation on models level

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,9 +1,15 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy.orm import DeclarativeBase, Mapped, validates, mapped_column, relationship
 from sqlalchemy import ForeignKey, text
 from uuid import UUID
 from datetime import datetime
 from typing import List
+from dotenv import load_dotenv
+from os import getenv
 
+load_dotenv()
+
+def validate_field_range():
+    """Soon. To get rid of repetative @validates logic"""
 
 class Base(DeclarativeBase):
     pass
@@ -20,8 +26,8 @@ class User(Base):
     __tablename__ = "users"
 
     user_id: Mapped[UUID] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str] = mapped_column(unique=True)
     password_hash: Mapped[str]
     joined: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
 
@@ -31,18 +37,7 @@ class User(Base):
         lazy="selectin"
     )
 
-    comments: Mapped[List["Comment"]] = relationship(
-        "Comment",
-        back_populates="owner",
-        lazy="selectin"
-    )
-
-    reposts: Mapped[List["Repost"]] = relationship(
-        "Repost",
-        back_populates="owner",
-        lazy="selectin"
-    )
-
+    # Self referable many-2-many https://stackoverflow.com/questions/9116924/how-can-i-achieve-a-self-referencing-many-to-many-relationship-on-the-sqlalchemy
     followed: Mapped[List["User"]] = relationship(
         "User",
         secondary="friendship", 
@@ -60,6 +55,12 @@ class User(Base):
         lazy="selectin"
     )
 
+    @validates("username")
+    def validate_username(self, key, username: str):
+        if not getenv("USERNAME_MIN_L") <= len(username) <= getenv("USERNAME_MAX_L"):
+            raise ValueError("Username length is out of range")
+        return username
+
     def __repr__(self):
         return f"Username: {self.username}"
 
@@ -69,51 +70,42 @@ class Post(Base):
 
     post_id: Mapped[UUID] = mapped_column(primary_key=True)
     owner_id: Mapped[UUID] = mapped_column(ForeignKey("users.user_id", ondelete="SET NULL"), nullable=True)
+    parent_post_id: Mapped[UUID| None] = mapped_column(ForeignKey("posts.post_id", ondelete="SET NULL"), nullable=True)
+
+    is_reply: Mapped[bool] = mapped_column(default=False)
 
     # Add constraits!!!
-    title: Mapped[str]
-    description: Mapped[str]
+    title: Mapped[str] = mapped_column(nullable=True)
     text: Mapped[str]
     image_path: Mapped[str] = mapped_column(nullable=True)
     likes: Mapped[int] = mapped_column(default=0)
     published: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
     last_updated: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"), onupdate=text("TIMEZONE('utc', now())"))
 
-    comments: Mapped[List["Comment"]] = relationship("Comment", back_populates="parent_post", lazy="selectin")
     owner: Mapped["User"] = relationship("User", back_populates="posts", lazy="selectin")
-    reposts: Mapped[List["Repost"]] = relationship("Repost", back_populates="parent_post", lazy="selectin")
 
+    # Self referable one-2-many relationship https://docs.sqlalchemy.org/en/20/orm/self_referential.html
+    parent_post: Mapped["Post"] = relationship(
+        "Post",
+        back_populates="replies",
+        remote_side=[post_id]
+    )
+    replies: Mapped[List["Post"]] = relationship(
+        "Post",
+        back_populates="parent_post",
+    )
 
-class Repost(Base):
-    __tablename__ = "reposts"
+    @validates("title")
+    def validate_title(self, key, title: str):
+        if not getenv("POST_TITLE_MIN_L") <= len(title) <= getenv("POST_TITLE_MAX_L"):
+            raise ValueError("Post title length is out of range")
+        return title
 
-    repost_id: Mapped[UUID] = mapped_column(primary_key=True)
-    parent_id: Mapped[UUID] = mapped_column(ForeignKey("posts.post_id", ondelete="SET NULL"), nullable=True)
-    owner_id: Mapped[UUID] = mapped_column(ForeignKey("users.user_id", ondelete="SET NULL"), nullable=True)
+    @validates("text")
+    def validate_text(self, key, text: str):
+        if not getenv("POST_TEXT_MIN_L") <= len(text) <= getenv("POST_TEXT_MAX_L"):
+            raise ValueError("Post text length is out of range")
+        return text
 
-    # Add constraits to fields
-    text: Mapped[str]
-    likes: Mapped[int] = mapped_column(default=0)
-    published: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
-    last_updated: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"), onupdate=text("TIMEZONE('utc', now())"))
-
-    parent_post: Mapped["Post"] = relationship("Post", back_populates="reposts", lazy="selectin")
-    owner: Mapped["User"] = relationship("User", back_populates="reposts", lazy="selectin")
-
-
-class Comment(Base):
-    __tablename__ = "comments"
-
-    comment_id: Mapped[UUID] = mapped_column(primary_key=True)
-    post_id: Mapped[UUID] = mapped_column(ForeignKey("posts.post_id", ondelete="CASCADE"), nullable=False)
-    owner_id: Mapped[UUID] = mapped_column(ForeignKey("users.user_id", ondelete="SET NULL"), nullable=True)
-    owner_username: Mapped[str] = mapped_column(ForeignKey("users.username", ondelete="SET NULL"), nullable=True)
-    parent_comment_id: Mapped[UUID] = mapped_column(ForeignKey("comments.comment_id", ondelete="CASCADE"), nullable=True)
-
-    text: Mapped[str]
-    published: Mapped[datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
-
-    parent_comment: Mapped["Comment"] = relationship("Comment", back_populates="replies", remote_side=[comment_id], lazy="selectin")
-    replies: Mapped[List["Comment"]] = relationship("Comment", back_populates="parent_comment", lazy="selectin")
-    parent_post: Mapped["Post"] = relationship("Post", back_populates="comments", lazy="selectin")
-    owner: Mapped["User"] = relationship("User", back_populates="comments", lazy="selectin")
+    def __repr__(self):
+        return f"Post name: {self.title}"
