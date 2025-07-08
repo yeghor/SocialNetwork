87074a929b08df69159d86342ad4dc6839aec381
RomanovYehor <yeghor.romanov@gmail.com>
2025-07-08 12:56:05 +0300

    Expanded tests. Now covers models rlationship

--- backend/database_tests.py
+++ backend/database_tests.py
@@ -7,6 +7,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from authorization.password_manager import hash_password
 from uuid import uuid4
+from typing import AsyncGenerator, List
+import logging
 
 from database.database_utils import (
     get_all_posts,
@@ -20,7 +22,39 @@ from database.database_utils import (
 """Tests postgres and chromaDB"""
 
 @pytest.fixture
-async def conn():
+def users() -> List[User]:
+    pass1 = hash_password("password1")
+    pass2 = hash_password("password2")
+    pass3 = hash_password("password3")
+
+    user_1 = User(username="user1", email="user1@gmail.com", password_hash=pass1)
+    user_2 = User(username="user2", email="user2@gmail.com", password_hash=pass2)
+    user_3 = User(username="user3", email="user3@gmail.com", password_hash=pass3)
+
+    user_1.followed.append(user_2)
+    user_1.followed.append(user_3)
+    user_2.followed.append(user_1)
+    user_3.followed.append(user_1)
+
+    return [user_1, user_2, user_3]
+
+@pytest.fixture
+def posts(users) -> List[Post]:
+    user_1, user_2, user_3 = users
+
+    post_1_id, post_2_id, post_3_id, post_4_id, post_5_id, post_6_id = (uuid4() for _ in range(6))
+
+    post_1 = Post(post_id=post_1_id, title="First post. Non reply", text="Hello, how is it going?", likes=42)
+    post_2 = Post(post_id=post_2_id, title="Second post. Reply to first. Reply", text="Fine! Thx for asking", likes=30, is_reply=True, parent_post_id=post_1.post_id)
+    post_3 = Post(post_id=post_3_id, title="Third post. Weekend plans. Non reply", text="Anyone up for hiking this weekend?", likes=15)
+    post_4 = Post(post_id=post_4_id, title="Fourth post. Reply to weekend plans. Reply", text="No :( Got plans for next weekend", likes=22, is_reply=True, parent_post_id=post_3.post_id)
+    post_5 = Post(post_id=post_5_id, title="Fifth post. Reply to weekend plans. Reply", text="Awesome! I'm with you", likes=18, is_reply=True, parent_post_id=post_3.post_id)
+    post_6 = Post(post_id=post_6_id, title="Sixth post (3 layer of replies)", text="I'll call you today.", likes=3, is_reply=True, parent_post_id=post_5.post_id)
+
+    return [post_1, post_2, post_3, post_4, post_5, post_6]
+
+@pytest.fixture
+async def conn(users: List[User], posts: List[Post]):
     """Creates connection and fills test database with fake data"""
     engine = create_engine(mode="test", echo=True)
     session = create_sessionmaker(engine=engine)
@@ -28,41 +62,47 @@ async def conn():
     await drop_all(engine=engine, Base=Base)
     await initialize_models(engine=engine, Base=Base)
 
+    user_1, user_2, user_3 = users
+    post_1, post_2, post_3, post_4, post_5, post_6 = posts
 
     async with session() as connection:
-        pass1 = hash_password("password1")
-        pass2 = hash_password("password2")
-        pass3 = hash_password("password3")
-
-        user_1 = User(username="user1", email="user1@gmail.com", password_hash=pass1)
-        user_2 = User(username="user2", email="user2@gmail.com", password_hash=pass2)
-        user_3 = User(username="user3", email="user3@gmail.com", password_hash=pass3)
-
-        user_1.followed.append(user_2)
-        user_1.followed.append(user_3)
-
-        user_2.followed.append(user_1)
-
-        user_3.followed.append(user_1)
-
-        connection.add_all([user_1, user_2, user_3])
+        connection.add_all(users)
         await connection.flush()
 
-        post_1 = Post(owner_id=user_1.user_id, title="First post. Non reply", text="Hello, how is it going?", likes=42)
-        post_2 = Post(owner_id=user_2.user_id, title="Second post. Reply to first. Reply", text="Fine! Thx for asking", likes=30, is_reply=True, parent_post_id=post_1.post_id)
-        post_3 = Post(owner_id=user_3.user_id, title="Third post. Weekend plans. Non reply", text="Anyone up for hiking this weekend?", likes=15)
-        post_4 = Post(owner_id=user_1.user_id, title="Fourth post. Reply to weekend plans. Reply", text="No :( Got plans for next weekend", likes=22, is_reply=True, parent_post_id=post_3.post_id)
-        post_5 = Post(owner_id=user_2.user_id, title="Fifth post. Reply to weekend plans. Reply", text="Awesome! I'm with you", likes=18, is_reply=True, parent_post_id=post_3.post_id)
-        post_6 = Post(owner_id=user_3.user_id, title="Sixth post (3 layer of replies)", text="I'll call you today.", likes=3, is_reply=True, parent_post_id=post_3.post_id)
+        post_1.owner_id = user_1.user_id 
+        post_2.owner_id = user_2.user_id
+        post_3.owner_id = user_3.user_id
+        post_4.owner_id = user_1.user_id
+        post_5.owner_id = user_2.user_id
 
-        connection.add_all([post_1, post_2, post_3, post_4, post_5, post_6])
+        connection.add_all(posts)
         await connection.flush()
 
         yield connection
 
 @pytest.mark.asyncio
-async def test_models(conn):
-    session = await anext(conn)
+async def test_models(conn, users: List[User], posts: List[Post]):
+    """Test relationship in priority"""
+
+    session: AsyncSession = await anext(conn)
     print(type(session))
-    all_users = await get_all_users(session=session)
-    all_posts = await get_all_posts(session=session)
+    all_users: List[User] = await get_all_users(session=session)
+    all_posts: List[Post] = await get_all_posts(session=session)
+    assert len(all_users) == len(users)
+    assert len(all_posts) == len(posts)
+
+    user_1, user_2, user_3 = users
+    post_1, post_2, post_3, post_4, post_5, post_6 = posts
+
+    assert user_1.followers == [user_2, user_3]
+    assert user_3.followers == [user_1]
+
+    assert user_2.posts == [post_2, post_5]
+
+    # Ensure that 3 level of post reply tree working properly
+    for post in all_posts:
+        if post.post_id == post_6.post_id:
+            post_6_query = post
+            break
+
+    assert post_6_query.parent_post.parent_post.post_id == post_3.post_id 
