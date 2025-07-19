fad4466f3c7c1f1fb1964326ec1c5fe11e17a662
RomanovYehor <yeghor.romanov@gmail.com>
2025-07-19 12:21:46 +0300

    Import ugfix

--- backend/databases_manager/main_managers/social_manager.py
+++ backend/databases_manager/main_managers/social_manager.py
@@ -1,11 +1,6 @@
-from databases_manager.chromaDB_manager.chroma_manager import ChromaService
-from databases_manager.postgres_manager.database_utils import PostgresService
-from databases_manager.redis_manager.redis_manager import RedisService
 from databases_manager.main_managers.main_manager_creator_abs import MainServiceBase
 from databases_manager.postgres_manager.models import User, Post, Base
-from authorization import jwt_manager
 
-from sqlalchemy.ext.asyncio import AsyncSession
 from dotenv import load_dotenv
 from os import getenv
 from typing import List, TypeVar, Type
